a9a8d8564e8208bf4c0cbee02b7a805291173413
pankaj singh chamyal <59091556+pankajsinghchamyal@users.noreply.github.com>
2020-12-09 15:05:09 +0530

    Update App1_Social_Distance.py

--- Deep Learning/Social Distancing app/App1_Social_Distance.py
+++ Deep Learning/Social Distancing app/App1_Social_Distance.py
@@ -1,6 +1,4 @@
 #================================================================
-#  To learn how to Develop Advance YOLOv4 Apps - Then check out:
-#  https://augmentedstartups.info/yolov4release
 #================================================================ 
 from ctypes import *
 import math
